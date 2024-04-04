839df9e3b20c64458bd1087973e6bdc17d547c3a
Jeevan Dhinsa <115588451+JeevanDhinsa@users.noreply.github.com>
2024-04-04 21:32:36 +0100

    Add files via upload

--- data_preproccesing script.py
+++ data_preproccesing script.py
@@ -1,7 +1,9 @@
 import splitfolders
 
+# Input and output folders
 input_folder = "star_wars_images"
 output = "star_wars_images_processed"
 
+# Splitting images in the folders into training,validation and testing with a 60/20/20 ratio
 splitfolders.ratio(input_folder,output, seed = 42, ratio=(.6,.2,.2))
 
